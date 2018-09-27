965d49c813e3a789dfd763c15f4b1b1f08792d11
Daria Primorskaya <riokko@rizn.org>
2018-09-27 20:29:06 +0300

    создаем БД

--- learn_sqlalchemy.py
+++ learn_sqlalchemy.py
@@ -9,3 +9,19 @@ db_session = scoped_session(sessionmaker(bind=engine))
 
 Base = declarative_base()
 Base.query = db_session.query_property()
+
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(String(50))
+    first_name = Column(String(50))
+    last_name = Column(String(50))
+    email = Column(String(120), unique=True)
+
+    def __init__(self, first_name=None, last_name=None, email=None):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+
+    def __repr__(self):
+        return '<User {} {}>'.formst(self.first_name, self.last_name)
